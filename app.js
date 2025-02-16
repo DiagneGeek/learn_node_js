872a493f6192f21998f87a16b1b0f2dc8a7c91bb
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-02-16 22:25:22 +0000

    first step

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const fs = require("fs")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.send(fs.readFileSync("index.html", 'utf8'))
+})
+
+app.get("/api/todos", (req, res) => {
+    fs.readFile("todos.json", 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send({ error: "Could not read todos.json" })
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
+})
+
+app.post("/api/todos", (req, res) => {
+    fs.readFile("todos.json", 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send({ error: "Could not read todos.json" })
+        } else {
+            const todos = JSON.parse(data)
+            const newTodo = {
+                id: todos.length + 1,
+                title: req.body.title,
+                completed: false
+            }
+            todos.push(newTodo)
+            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+                if (err) {
+                    res.status(500).send({ error: "Could not write to todos.json" })
+                } else {
+                    res.json(newTodo)
+                }
+            })
+        }
+    })
+})
+
+app.delete("/api/todos/:id", (req, res) => {
+    fs.readFile("todos.json", 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send({ error: "Could not read todos.json" })
+        } else {
+            let todos = JSON.parse(data)
+            todos = todos.filter(todo => todo.id !== parseInt(req.params.id))
+            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+                if (err) {
+                    res.status(500).send({ error: "Could not write to todos.json" })
+                } else {
+                    res.sendStatus(204)
+                }
+            })
+        }
+    })
+})
+
+app.listen(3009, () => {
+    console.log("Server is running on port 3000")
+})
