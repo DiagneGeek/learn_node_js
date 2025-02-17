255c2caa12272e244c0bc1b69cd6afff12355a2a
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-02-17 17:19:55 +0000

    now i have a social network

--- api/index.js
+++ api/index.js
@@ -6,12 +6,19 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
 app.get("/", (req, res) => {
+    res.type("text/html")
     res.send(fs.readFileSync("index.html", 'utf8'))
 })
 
-app.get("/api/todos", (req, res) => {
-    fs.readFile("todos.json", 'utf8', (err, data) => {
+app.get("/addPost", (req, res) => {
+    res.type("text/html")
+    res.send(fs.readFileSync("addPost.html", 'utf8'))
+})
+
+app.get("/api/posts", (req, res) => {
+    fs.readFile("posts.json", 'utf8', (err, data) => {
         if (err) {
             res.status(500).send({ error: "Could not read todos.json" })
         } else {
@@ -20,37 +27,39 @@ app.get("/api/todos", (req, res) => {
     })
 })
 
-app.post("/api/todos", (req, res) => {
-    fs.readFile("todos.json", 'utf8', (err, data) => {
+app.post("/api/posts", (req, res) => {
+    fs.readFile("posts.json", 'utf8', (err, data) => {
         if (err) {
             res.status(500).send({ error: "Could not read todos.json" })
         } else {
-            const todos = JSON.parse(data)
-            const newTodo = {
-                id: todos.length + 1,
+            let posts = JSON.parse(data)
+            const newPost = {
+                id: posts.length + 1,
                 title: req.body.title,
-                completed: false
+                body: req.body.content
             }
-            todos.push(newTodo)
-            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+            posts.push(newPost)
+            posts = [...new Set(posts)]
+            fs.writeFile("posts.json", JSON.stringify(posts), (err) => {
                 if (err) {
                     res.status(500).send({ error: "Could not write to todos.json" })
                 } else {
-                    res.json(newTodo)
+                    res.type("json")
+                    res.json(newPost)
                 }
             })
         }
     })
 })
 
-app.delete("/api/todos/:id", (req, res) => {
-    fs.readFile("todos.json", 'utf8', (err, data) => {
+app.delete("/api/posts/:id", (req, res) => {
+    fs.readFile("posts.json", 'utf8', (err, data) => {
         if (err) {
-            res.status(500).send({ error: "Could not read todos.json" })
+            res.status(500).send({ error: "Could not read posts.json" })
         } else {
-            let todos = JSON.parse(data)
-            todos = todos.filter(todo => todo.id !== parseInt(req.params.id))
-            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+            let posts = JSON.parse(data)
+            posts = posts.filter(post => post.id !== parseInt(req.params.id))
+            fs.writeFile("posts.json", JSON.stringify(posts), (err) => {
                 if (err) {
                     res.status(500).send({ error: "Could not write to todos.json" })
                 } else {
@@ -61,6 +70,10 @@ app.delete("/api/todos/:id", (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('App running on port 3000'))
+let port = 3014
+
+app.listen(port, () => {
+    console.log('App running on port ' + port)
+})
 
 module.exports = app
